1658a0762fdae5b060bfbda85e67db010d31fb02
Chris Berger <cberger8@yahoo.com>
2015-11-28 01:24:52 -0500

    ATM command base implemented

--- ATM.cpp
+++ ATM.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h> //printf
 #include <string.h>    //strlen
 #include <string>  //string
+#include <sstream> //stringstream
 #include <sys/socket.h>    //socket
 #include <arpa/inet.h> //inet_addr
 #include <netdb.h> //hostent
@@ -10,7 +11,127 @@
 using namespace std;
 
 #include "tcp_client.h"
- 
+
+bool loggedIn = false;
+string currentUser;
+tcp_client c;
+stringstream sstm;
+
+bool encryptAndSend(string msg) {
+	
+	// encryption
+	string encrypted_msg = msg;
+
+	return c.send_data(encrypted_msg);
+}
+
+string receiveAndDecrypt() {
+
+	string resp = c.receive(512);
+
+	// decryption
+	string decrypted_msg = resp;
+
+	return decrypted_msg;
+}
+
+void login(int portNum) {
+	if(loggedIn) {
+		cout << "Already logged in" << endl;
+		return;
+	}
+	string username;
+	cin >> username;
+	if(username == "") {
+		cout << "No username entered" << endl;
+		return;
+	}
+
+	int pinNum;
+	cout << "Enter PIN: ";
+	cin >> pinNum;
+
+	// Authentication
+
+	c.conn("localhost", portNum);
+	loggedIn = true;
+	currentUser = username;
+}
+
+void balance() {
+	if(!loggedIn) {
+		cout << "Not logged in" << endl;
+		return;
+	}
+	
+	sstm.str("");
+	sstm << currentUser << " BALANCE";
+	string msg = sstm.str();
+	if( !encryptAndSend(msg) ) {
+		cerr << "ERROR: Could not contact server" << endl;
+	}
+	else {
+		cout << receiveAndDecrypt() << endl;
+	}
+}
+
+void withdraw() {
+	if(!loggedIn) {
+		cout << "Not logged in" << endl;
+		return;
+	}
+	int amt = -1;
+	cin >> amt;
+	if( amt < 0 ) {
+		cout << "Invalid amount" << endl;
+		return;
+	}
+
+	// funds check
+
+	sstm.str("");
+	sstm << currentUser << " WITHDRAW " << amt;
+	string msg = sstm.str();
+	if( !encryptAndSend(msg) ) {
+		cerr << "ERROR: Could not contact server" << endl;
+	}
+	else {
+		cout << receiveAndDecrypt() << endl;
+	}
+}
+
+void transfer() {
+	if(!loggedIn) {
+		cout << "Not logged in" << endl;
+		return;
+	}
+	int amt = -1;
+	cin >> amt;
+	if( amt < 0 ) {
+		cout << "Invalid amount" << endl;
+		return;
+	}
+	string recAcct = "";
+	cin >> recAcct;
+	if( recAcct == "" ) {
+		cout << "Receiver account not entered" << endl;
+		return;
+	}
+
+	// account verification check
+	// funds check
+
+	sstm.str("");
+	sstm << currentUser << " TRANSFER " << amt << " " << recAcct;
+	string msg = sstm.str();
+	if( !encryptAndSend(msg) ) {
+		cerr << "ERROR: Could not contact server" << endl;
+	}
+	else {
+		cout << receiveAndDecrypt() << endl;
+	}
+}
+
 int main(int argc , char *argv[])
 {
 	if( argc != 2 ) {
@@ -19,21 +140,19 @@ int main(int argc , char *argv[])
 	}
 
 	int portNum = atoi(argv[1]);
+	string data, encryptedData;
 
-	//connect to host
-    tcp_client c;
-    c.conn("localhost", portNum);
+	string command;
+	while(true) {
+		cout << "ATM $";
+		cin >> command;
 
-	/* Enter user */
-    string userName;
-    cout<<"\nEnter your user name: ";
-    cin>>userName;
-
-    // Check if user exists
-
-    int pinNum;
-    cout<<"Enter your PIN number: ";
-    cin>>pinNum;
+		if(command == "login") login(portNum);
+		else if(command == "balance") balance();
+		else if(command == "withdraw") withdraw();
+		else if(command == "transfer") transfer();
+		//else if(command == "logout") logout();
+	}
 
 
 //namecard??
@@ -62,17 +181,17 @@ int main(int argc , char *argv[])
       send_data (Operator):
       http://www.halcon.com/download/reference/send_data.html
     */
-    c.send_data("hello");
+    /*c.send_data("hello");
      
     //receive and echo reply
     cout<<"----------------------------\n\n";
     cout<<c.receive(1024);
-    cout<<"\n\n----------------------------\n\n";
+    cout<<"\n\n----------------------------\n\n";*/
      
 
-    string command;
+    /*string command;
     cout<<"Hello, how can I help you today? (balance, withdraw, transfer or logout)\n";
-    cin>>command;
+    cin>>command;*/
 
 //if statement...
 
