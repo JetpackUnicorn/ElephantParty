11c32a016fc652bff306889d2a02f2c5cda00639
JetpackUnicorn <hwangkevj@gmail.com>
2015-12-03 21:55:31 -0500

    Made crypto.cpp

---
+++ crypto.cpp
@@ -0,0 +1,78 @@
+#include "crypto.h"
+
+using namespace CryptoPP;
+using std::string;
+
+void Crypto::SharedKeyInit(const std::string & publicKeyFile)
+{  
+  // InvertibleRSAFunction is used directly only because the private key
+  // won't actually be used to perform any cryptographic operation;
+  // otherwise, an appropriate typedef'ed type from rsa.h would have been used.
+  AutoSeededRandomPool rng;
+  InvertibleRSAFunction privkey;
+  privkey.Initialize(rng, 1024);
+  PrivateKey = privkey;
+  
+  // Suppose we want to store the public key separately,
+  // possibly because we will be sending the public key to a third party.
+  RSAFunction pubkey(privkey);
+  const char * publicKeyFileCString = publicKeyFile.c_str();
+  Base64Encoder pubkeysink(new FileSink(publicKeyFileCString));
+  pubkey.DEREncode(pubkeysink);
+  pubkeysink.MessageEnd(); 
+}
+
+void Crypto::LoadOppositeKey(const std::string & oppositePublicKeyFile)
+{
+    ByteQueue bytes;
+    const char * publicKeyFileCString = oppositePublicKeyFile.c_str();
+    FileSource file(publicKeyFileCString, true, new Base64Decoder);
+    file.TransferTo(bytes);
+    bytes.MessageEnd();
+    OppositePublicKey.Load(bytes);
+}
+
+string Crypto::RSAEncryption(const string & plainTxt)
+{
+    AutoSeededRandomPool rng;
+    RSAES_OAEP_SHA_Encryptor e(OppositePublicKey);
+    string cipherTxt;
+    StringSource ss1(plainTxt, true, new PK_EncryptorFilter(rng, e, new StringSink(cipherTxt)));
+    return cipherTxt;
+}
+
+string Crypto::RSADecryption(const string & cipherTxt)
+{
+    AutoSeededRandomPool rng;
+    std::string plainTxt;
+    RSAES_OAEP_SHA_Decryptor d(PrivateKey);
+    StringSource ss2(cipherTxt, true, new PK_DecryptorFilter(rng, d, new StringSink(plainTxt)));
+    return plainTxt;
+}
+
+std::string Crypto::SignatureSign(const string & msg)
+{
+    string message = msg;
+    AutoSeededRandomPool rng;
+    RSASS<PSSR, SHA1>::Signer signer(PrivateKey);
+    std::string signature;
+    StringSource(message, true, new SignerFilter(rng, signer, new StringSink(signature), true));
+    return signature;
+}
+
+string Crypto::SignatureVerify(const string & signature){
+    string recovered, message;
+    ByteQueue bytes;
+    OppositePublicKey.Load(bytes);
+    RSASS<PSSR, SHA1>::Verifier verifier(OppositePublicKey);
+    StringSource(signature, true,
+               new SignatureVerificationFilter(
+                                               verifier,
+                                               new StringSink(recovered),
+                                               SignatureVerificationFilter::THROW_EXCEPTION |
+                                               SignatureVerificationFilter::PUT_MESSAGE
+                                               ) // SignatureVerificationFilter
+               ); // StringSource
+    return recovered;
+}
+
---
+++ crypto.h
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <string>
+#include <cryptopp/base64.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/integer.h>
+#include <cryptopp/files.h>
+#include <cryptopp/osrng.h>
+#include <cryptopp/pssr.h>
+#include <cryptopp/rsa.h>
+#include <cryptopp/filters.h>
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/sha.h>
+
+using namespace CryptoPP;
+using std::string;
+
+class Crypto {
+private:
+    RSA::PrivateKey PrivateKey;
+    RSA::PublicKey PublicKey;
+    RSA::PublicKey OppositePublicKey;   
+public:
+    Crypto(){};
+    RSA::PrivateKey GetPrivateKey() { return PrivateKey; }
+    RSA::PublicKey GetPublicKey() { return PublicKey; }
+    RSA::PublicKey GetOppositePublicKey() { return OppositePublicKey; }
+    void SharedKeyInit(const string & publicKeyFile);
+    void LoadOppositeKey(const std::string & oppositePublicKeyFile);
+    string RSAEncryption(const string & plainTxt);
+    string RSADecryption(const string & cipherTxt);
+    string SignatureSign(const string & msg);
+    string SignatureVerify(const string & signature);
+};
